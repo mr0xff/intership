625aa21735524404d0bbef90aadf02f26430245a
unknown <braulialueji8@gmail.com>
2024-12-29 03:02:37 +0100

    Remoção do navbar no Header

--- OnlineDictionary/frontend/src/app/components/Header.tsx
+++ OnlineDictionary/frontend/src/app/components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import Navbar from "./Navbar";
 
 const Header: React.FC = () => {
   return (
@@ -8,18 +7,15 @@ const Header: React.FC = () => {
       <div className=" flex justify-between items-center">
         
         <div className="flex items-center space-x-4">
-          <img src="/DicioLuna.webp" alt="DicioLuna Logo" className="h-12 w-12"/>
+
           <div>
             <h1 className="text-2xl font-bold">DicioLuna</h1>
             <p className="text-sm">Dicionário online</p>
           </div>
         </div>
-      {/**  <div className="text-right">
-          <p>Projeto de estagio</p>
-          <p>Nominated 2024</p>
-        </div>**/}
+
       </div>
-      <Navbar />
+
     </header>
   );
 };
