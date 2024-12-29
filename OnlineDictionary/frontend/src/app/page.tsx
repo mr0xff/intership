b18d5dfdaa116c10bc547f673f84ceb8f6a0eda7
unknown <braulialueji8@gmail.com>
2024-12-29 03:34:50 +0100

    Implementa a  estrutura página inicial

--- OnlineDictionary/frontend/src/app/page.tsx
+++ OnlineDictionary/frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import Header from "./components/Header";
-import Search from "./components/Search";
+import Search from "./components/Search"
 import Footer from "./components/Footer";
 import Curiosity from './components/Curiosity';
 
